08d7f257f1996d3049134aea95a58573c8990b12
catmandoe1 <73958516+catmandoe1@users.noreply.github.com>
2022-03-18 23:56:33 +0000

    changed some info things
    still only has 3 items

---
+++ java/s11/mod/Main.java
@@ -0,0 +1,42 @@
+package s11.mod;
+
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import s11.mod.proxy.CommonProxy;
+import s11.mod.recipes.SmeltingInit;
+import s11.mod.tabs.PollutionPlusTab;
+import s11.mod.util.Reference;
+import s11.mod.util.handlers.RegisteryHandler;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+	@Instance
+	public static Main instance;
+	
+	public static final CreativeTabs pollutionplustab = new PollutionPlusTab("pollutionplustab");
+		
+	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {
+		RegisteryHandler.otherRegisteries();}
+
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {
+		RegisteryHandler.initRegistries();
+		SmeltingInit.initSmelting();
+	}
+		
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
---
+++ java/s11/mod/world/gen/WorldGenCustomOres.java
@@ -0,0 +1,54 @@
+package s11.mod.world.gen;
+
+import java.util.Random;
+
+import net.minecraft.block.state.pattern.BlockMatcher;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+import s11.mod.init.BlockInit;
+
+public class WorldGenCustomOres implements IWorldGenerator{
+	private WorldGenerator ore_void;
+	
+	public WorldGenCustomOres() {
+		// Black ore
+		ore_void = new WorldGenMinable(BlockInit.ORE_VOID.getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+	}
+	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+		switch(world.provider.getDimension()) {
+		case -1:
+			// Nether Ores
+			break;
+			
+		case 0:
+			// Overworld Ores
+			runGenerator(ore_void, world, random, chunkX, chunkZ, 1, 5, 30);
+			break;
+		case 1:
+			// End Ores
+			break;
+		}
+	}
+	
+	private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, int chance, int minHeight, int maxHeight) {
+		if(minHeight > maxHeight || minHeight < 0 || maxHeight > 256) throw new IllegalArgumentException("Ore generated out of bounds");
+		
+		int heightDiff = maxHeight - minHeight + 1;
+		for(int i = 0; i < chance; i++) {
+			int x = chunkX * 16 + rand.nextInt(16);
+			int y = minHeight + rand.nextInt(heightDiff);
+			int z = chunkZ * 16 + rand.nextInt(16);
+			
+			gen.generate(world, rand, new BlockPos(x, y, z));
+		}
+		
+	}
+}
